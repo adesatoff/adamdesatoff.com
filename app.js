ba8bbccca2351d88e23f39c27329f66caabfd026
Adam Desatoff <ad.desatoff@gmail.com>
2023-11-03 15:02:10 -0600

    11/3/23

--- app.js
+++ app.js
@@ -1,7 +1,52 @@
-Vue.createApp({
+const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      name: "Adam Desatoff",
+      typedName: "",
+      job: "Computer Science Student",
+      typedJob: "",
+      blink: false,
+    };
   },
-  methods: {},
-  created: function () {},
-}).mount("#app");
+  methods: {
+    typeName() {
+      let i = 0;
+      const nameLength = this.name.length;
+      const typeInterval = 75;
+      const typeTimer = setInterval(() => {
+        if (i < nameLength) {
+          this.typedName += this.name[i];
+          i++;
+        } else {
+          clearInterval(typeTimer);
+        }
+      }, typeInterval);
+    },
+    typeJob() {
+      let i = 0;
+      const nameLength = this.job.length;
+      const typeInterval = 60;
+      const typeTimer = setInterval(() => {
+        if (i < nameLength) {
+          this.typedJob += this.job[i];
+          i++;
+        } else {
+          clearInterval(typeTimer);
+          setTimeout(() => {
+            this.blink = true;
+          }, 500);
+        }
+      }, typeInterval);
+    },
+  },
+  created() {
+    setTimeout(() => {
+      this.typeName();
+    }, 500);
+    setTimeout(() => {
+      this.typeJob();
+    }, 2250);
+  },
+});
+
+app.mount("#app");
